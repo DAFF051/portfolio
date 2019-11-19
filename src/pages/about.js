5c373b54908b5705fd1ca77b502fbffe7706b282
mo <mohammed@artlimes.com>
2019-11-19 20:32:02 +0200

    set routs

---
+++ src/pages/about.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function About () {
+  return <h1 className="main border border-danger">this about page</h1>;
+}
+
+export default About;
---
+++ src/pages/home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Welcome () {
+  return <h1 className="main border border-danger">Hello World from React boilerplate</h1>;
+}
+
+export default Welcome;
